f6093cb2ff204aa8157ff900450153f3285889b6
Kelly Gravelyn <kelly@kellyrenee.me>
2022-05-30 13:07:19 -0700

    Move Windows Terminal off of ERB

--- generator/generators/windows-terminal/windows_terminal_generator.rb
+++ generator/generators/windows-terminal/windows_terminal_generator.rb
@@ -1,7 +1,36 @@
 class WindowsTerminalGenerator
-  include ErbGenerator
+  include Generator
   include TerminalColorSet
 
   def name; 'windows-terminal'; end
-  def source_dir; __dir__; end
+
+  def generate
+    color_scheme = {
+      "name" => "Raindrop",
+      "background" => background.to_s,
+      "foreground" => foreground.to_s,
+      "cursorColor" => cursor.to_s,
+      "selectionBackground" => selection.to_s,
+
+      "black" => Color::BLACK.to_s,
+      "brightBlack" => Color::BLACK.to_s,
+      "red" => Color::RED.to_s,
+      "brightRed" => Color::RED.to_s,
+      "green" => Color::GREEN.to_s,
+      "brightGreen" => Color::GREEN.to_s,
+      "yellow" => Color::YELLOW.to_s,
+      "brightYellow" => Color::YELLOW.to_s,
+      "blue" => Color::BLUE.to_s,
+      "brightBlue" => Color::BLUE.to_s,
+      "purple" => Color::PURPLE.to_s,
+      "brightPurple" => Color::PURPLE.to_s,
+      "cyan" => Color::BLUE.to_s,
+      "brightCyan" => Color::BLUE.to_s,
+      "white" => Color::WHITE.to_s,
+      "brightWhite" => Color::WHITE.to_s,
+    }
+
+    File.write(File.join(out_dir, 'Raindrop.WindowsTerminal.json'), JSON.pretty_generate(color_scheme))
+  end
+
 end
